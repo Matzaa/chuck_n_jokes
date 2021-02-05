bccf59bb6899e49fff4ac59a91705ec0bc72505a
Matzaa Wahorn <matzaa@matzaa.com>
2021-02-05 18:16:39 +0100

    user can filter jokes

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,14 +1,25 @@
 <template>
-    <div>
-        <b-pagination
-            v-model="currentPage"
-            :items="joke_list"
-            :per-page="10"
-            :total-rows="rows"
-            aria-controls="my-table"
-        ></b-pagination>
+    <div id="list" v-if="jokeList">
+        <div>
+            <b-pagination
+                v-model="currentPage"
+                :items="currentDisplay"
+                :per-page="10"
+                :total-rows="rows"
+                aria-controls="my-table"
+            ></b-pagination>
+            <b-dropdown text="filter by category">
+                <b-dropdown-item
+                    v-for="cat in jokeCategories"
+                    v-bind:key="cat"
+                    @click="filterList(cat)"
+                    >{{ cat }}</b-dropdown-item
+                >
+                <b-dropdown-item @click="showAll()">show all</b-dropdown-item>
+            </b-dropdown>
+        </div>
         <b-table
-            :items="joke_list"
+            :items="currentDisplay"
             :per-page="10"
             :current-page="currentPage"
         ></b-table>
@@ -20,54 +31,83 @@ export default {
     name: "List",
     data() {
         return {
-            joke_list: [],
+            jokeList: [],
             currentPage: 1,
+            currentDisplay: [],
+            jokeCategories: [],
+            filteredList: [],
         };
     },
     mounted() {
-        let categories = [
-            "animal",
-            "career",
-            "celebrity",
-            "dev",
-            "explicit",
-            "fashion",
-            "food",
-            "history",
-            "money",
-            "movie",
-            "music",
-            "political",
-            "religion",
-            "science",
-            "sport",
-            "travel",
-        ];
-        for (let i = 0; i < 70; i++) {
-            let randomNumber =
-                Math.floor(Math.random() * 10) + (categories.length - 10);
-            console.log("randomnum", randomNumber);
-            fetch(
-                `https://api.chucknorris.io/jokes/random?category=${categories[randomNumber]}`
-            )
-                .then((res) => res.json())
-                .then((joke) => {
-                    console.log("category joke", joke);
-                    let newJoke = {
-                        joke: joke.value,
-                        category: joke.categories[0],
-                    };
-                    this.joke_list.push(newJoke);
-                });
-        }
-        console.log("this list", this.joke_list);
+        // As there is no get request specified by the chucknorris api to get all of the jokes, and the request for random jokes doesn't
+        // contain the joke's category, I'm first fetching all of the categories and retrieving jokes based on category which is randomly
+        // set. As I cannot know the number of all the jokes, I arbitrarily set it to 150 requests so that it should give enough answers.
+        // I then filtered out duplicates.
+
+        fetch("https://api.chucknorris.io/jokes/categories")
+            .then((res) => res.json())
+            .then((categories) => {
+                this.jokeCategories = categories;
+
+                for (let i = 0; i < 150; i++) {
+                    let randomNumber = Math.floor(
+                        Math.random() * this.jokeCategories.length
+                    );
+                    fetch(
+                        `https://api.chucknorris.io/jokes/random?category=${this.jokeCategories[randomNumber]}`
+                    )
+                        .then((res) => res.json())
+                        .then((joke) => {
+                            let double = false;
+                            if (this.jokeList.length > 1) {
+                                this.jokeList.forEach((element) => {
+                                    if (element.joke == joke.value) {
+                                        console.log("it's duplicate");
+                                        double = true;
+                                        return double;
+                                    }
+                                });
+                            }
+                            if (!double) {
+                                let newJoke = {
+                                    joke: joke.value,
+                                    category: joke.categories[0],
+                                };
+                                this.jokeList.push(newJoke);
+                                this.currentDisplay = this.jokeList;
+                            }
+                        })
+                        .catch((err) =>
+                            console.log("error in fetching jokes", err)
+                        );
+                }
+                console.log("this jokelist", this.jokeList);
+            })
+            .catch((err) =>
+                console.log("error in fetsching categories: ", err)
+            );
     },
     computed: {
         rows() {
-            return this.joke_list.length;
+            return this.currentDisplay.length;
+        },
+    },
+    methods: {
+        filterList: function(categoryName) {
+            this.filteredList = this.jokeList.filter(
+                (joke) => joke.category === categoryName
+            );
+            this.currentDisplay = this.filteredList;
+        },
+        showAll: function() {
+            this.currentDisplay = this.jokeList;
         },
     },
 };
 </script>
 
-<style></style>
+<style>
+#list {
+    padding: 100px;
+}
+</style>
